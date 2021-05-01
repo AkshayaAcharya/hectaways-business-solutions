e5432b1a57247ccb57f935a6cbc1ee8303d5b795
AkshayaAcharya <acharya.akshaya04@gmail.com>
2021-05-01 12:56:46 +0530

    initial commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 const About = () => {
+    const rootStyle = {
+        height: '100vh',
+        'min-height' : '100vh'
+    }
     return(
-        <div className="container">
+        <div className="container" style={rootStyle}>
             <h2>Small description about our stratup</h2>
         </div>
     )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,9 +3,15 @@ import About from './About';
 import Services from './Services';
 import Contact from './Contact';
 const Home = () => {
+    const rootStyle = {
+        height: '100vh',
+        'min-height' : '100vh'
+    }
     return(
         <div className="container">
+            <div style={rootStyle}>
             <h2>Carousel to be added</h2>
+            </div>
             <About />
             <Services />
             <Contact />
